afc32aac42fb4dee3a6a81da1314120a99cadf36
Arthur.Lazarenko <30784593+Arthur-Lazarenko@users.noreply.github.com>
2018-12-03 14:28:43 +0300

    Инициализация репозитория.

---
+++ src/main/java/com/example/checkpointStatuses/checkpointStatuses/CheckpointStatusesApplication.java
@@ -0,0 +1,12 @@
+package com.example.checkpointStatuses.checkpointStatuses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckpointStatusesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckpointStatusesApplication.class, args);
+	}
+}
